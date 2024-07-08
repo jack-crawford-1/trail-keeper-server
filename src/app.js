99c59c12538e8b673823f3211266771c4eced9cc
Jack Crawford <primes_unit.05@icloud.com>
2024-07-08 23:42:04 +1200

    Express backend with cors, nodemon

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import cors from 'cors'
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import router from '../src/routes/index.js'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use('/v1', router)
+
+app.use((err, req, res, next) => {
+  res.status(400).json({ error: err.stack })
+})
+
+export default app
