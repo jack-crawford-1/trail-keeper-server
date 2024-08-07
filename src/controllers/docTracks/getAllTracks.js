db179e7699fb1a21ab87a155cb02f765ae82d510
Jack Crawford <primes_unit.05@icloud.com>
2024-08-07 16:02:49 +1200

    fixed error getting env variables

--- src/controllers/docTracks/getAllTracks.js
+++ src/controllers/docTracks/getAllTracks.js
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 let cachedTracks = null;
 
 const docApiKey = process.env.DOC_API_KEY;
 const url = `https://api.doc.govt.nz/v1/tracks`;
-console.log(docApiKey);
 
 export default async function getAllTracks(req, res) {
   if (cachedTracks) {
@@ -16,14 +18,19 @@ export default async function getAllTracks(req, res) {
         'x-api-key': docApiKey,
       },
     });
-    const data = await response.json();
 
+    if (!response.ok) {
+      throw new Error(`API error: ${response.statusText}`);
+    }
+
+    const data = await response.json();
     cachedTracks = data;
     res.status(200).json(data);
   } catch (error) {
     console.error('Error:', error);
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      message: 'Error fetching all tracks',
+      error: error.message,
+    });
   }
 }
-
-getAllTracks({}, { status: () => ({ json: () => {} }) });
